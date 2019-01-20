0e3a0cbd3fb55b4d6bb3c9606b9a4a06e36d3596
omiarefin <43521921+omiarefin@users.noreply.github.com>
2019-01-20 22:39:00 +0100

    Updated python code

--- Part4/python/foo.py
+++ Part4/python/foo.py
@@ -3,29 +3,34 @@
 
 from threading import Thread
 
-
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
+
 i = 0
 
 def incrementingFunction():
     global i
     # TODO: increment i 1_000_000 times
+    for j in range (0,1000000):
+      i=i+1
 
 def decrementingFunction():
     global i
     # TODO: decrement i 1_000_000 times
-
-
+    for j in range (0,1000000):
+      i=i-1
 
 def main():
     # TODO: Something is missing here (needed to print i)
-
+    #What is needed to do cnt figure out and i is being printed out
+    
     incrementing = Thread(target = incrementingFunction, args = (),)
     decrementing = Thread(target = decrementingFunction, args = (),)
     
     # TODO: Start both threads
+    incrementing.start()
+    decrementing.start()
     
     incrementing.join()
     decrementing.join()
